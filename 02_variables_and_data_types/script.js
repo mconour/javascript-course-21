3186c0bb298fdf5fc8419d0d49b2c7fb8e41a4c0
mconour <michaelconour@gmail.com>
2021-09-16 11:33:03 -0700

    update with latest changes to script.js

---
+++ 02_variables_and_data_types/script.js
@@ -0,0 +1,13 @@
+// strings
+const singleQuotes = 'Hello, world!';
+const doubleQuotes = "What up, world!";
+const backticks = `${2 + 2}`;
+
+
+// numbers
+const firstNum = `hello`;
+const secondNum = 10;
+const result = firstNum / secondNum;
+console.log(typeof result);
+
+// booleans
